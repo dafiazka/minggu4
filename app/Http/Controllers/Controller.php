47d66fe00d089fe3488c11d36f65d65a03e0ece5
dafiazka <2031710110@student.polinema.ac.id>
2021-09-28 15:46:34 +0700

    minggu4

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,19 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
-use App\Models\Post;
 
-class Controller extends Controller
+class Controller extends BaseController
 {
-    public function getAll(){
-        $post = Post::all();
-        return view('home', ['post=> $post']);
-    }
-
-    public function getById($id){
-        $post = Post::find($id);
-        return view('post', ['post=> $post']);
-    }
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
